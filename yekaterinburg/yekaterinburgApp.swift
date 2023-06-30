f86634d1e67df3ee430c21f9627395efcf885a49
Alexander Rohrig <me@alexanderrohrig.com>
2023-06-30 12:16:21 -0400

    Initial Commit

---
+++ yekaterinburg/yekaterinburgApp.swift
@@ -0,0 +1,17 @@
+//
+//  yekaterinburgApp.swift
+//  yekaterinburg
+//
+//  Created by Alexander Rohrig on 6/30/23.
+//
+
+import SwiftUI
+
+@main
+struct yekaterinburgApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
