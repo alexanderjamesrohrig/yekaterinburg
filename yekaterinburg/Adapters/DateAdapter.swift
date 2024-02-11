fe2886e3ea841265817f22b2f7ce5deeb85bd5a6
Alexander Rohrig <me@alexanderrohrig.com>
2024-02-10 23:06:07 -0500

    Added DateAdapter functions

--- yekaterinburg/Adapters/DateAdapter.swift
+++ yekaterinburg/Adapters/DateAdapter.swift
@@ -6,9 +6,11 @@
 //
 
 import Foundation
+import OSLog
 
 struct DateAdapter {
     private static let dateFormatter = DateFormatter()
+    private static let logger = Logger(subsystem: GeneralSecretary.shared.subsystem, category: "DateAdapter")
     /// Translates String (Ex. 2023-07-01T00:05:00Z) to formatted date String
     static func timeFrom(gameDate: String, withDate showing: Bool) -> String {
         dateFormatter.timeZone = TimeZone(abbreviation: "UTC")
@@ -23,13 +25,13 @@ struct DateAdapter {
         }
         dateFormatter.timeZone = .autoupdatingCurrent
         
-        return dateFormatter.string(from: inputAsDate ?? Date())
+        return dateFormatter.string(from: inputAsDate ?? Date.now)
     }
     /// Translates Date to yyyy-MM-dd String
     static func dateForAPI(date: Date) -> String {
         dateFormatter.dateFormat = "yyyy-MM-dd"
         dateFormatter.timeZone = .autoupdatingCurrent
-        let today = Date()
+        let today = Date.now
         return dateFormatter.string(from: today)
     }
     /// Translates String (Ex. 2023-07-01T00:05:00Z) to Date
@@ -37,25 +39,39 @@ struct DateAdapter {
         dateFormatter.timeZone = TimeZone(abbreviation: "UTC")
         dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss'Z'"
         let toDate = dateFormatter.date(from: date)
-        return toDate ?? Date()
+        return toDate ?? Date.now
     }
-    
-    /// Returns Date given ISO date String
+    /// Returns Date given String ISO (Ex. 2024-02-11T00:48:28Z) date
     static func dateFromISO(date: String) -> Date {
         return dateFromAPI(date: date)
     }
-    
-    /// Returns 
+    /// Returns String short formatted date and time given Date
     static func lastUpdateString(from date: Date) -> String {
         let df = DateFormatter()
         df.dateStyle = .short
         df.timeStyle = .short
         return df.string(from: date)
     }
-    
-    static func YeFormatString(from date: Date) -> String {
+    /// Returns String Yekaterinbug specific formatted date (Ex. Feb 10 (Sat)) given Date
+    static func yeFormatString(from date: Date) -> String {
         let df = DateFormatter()
         df.dateFormat = "MMM d (EEE)"
         return df.string(from: date)
     }
+    /// Returns Int year from given Date. Defaults to current year.
+    static func yearFrom(date: Date = Date.now) -> Int {
+        return Calendar.autoupdatingCurrent.component(.year, from: date)
+    }
+    /// Returns String year written out in words given Date. Defaults to current year.
+    static func yearInWordsFrom(year: Int = yearFrom()) -> String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .spellOut
+        let writtenYear = numberFormatter.string(from: NSNumber(integerLiteral: year))
+        guard let writtenYear = writtenYear else {
+            logger.error("Unable to format number in spell out style")
+            return ""
+        }
+        return writtenYear
+    }
+    private init() {}
 }
