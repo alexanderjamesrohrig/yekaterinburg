cc5bbf0f7c58713821d1e6010cb8cf36cf766bfd
Alexander Rohrig <me@alexanderrohrig.com>
2024-02-10 23:25:38 -0500

    Added Sample Manager

--- yekaterinburg/Managers/SampleManager.swift
+++ yekaterinburg/Managers/SampleManager.swift
@@ -8,8 +8,8 @@
 import Foundation
 
 class SampleManager {
-    
     static let shared = SampleManager()
-
     private init() {}
+    // Colorado Avalanche 1 at New York Rangers 2, 2024-02-05, Overtime, MSG, ESPN New York 98.7 FM
+    // New York Knicks at Memphis Grizzlies, 2024-02-06, MSG, ESPN New York 1050 AM
 }
