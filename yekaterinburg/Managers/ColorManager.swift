cb5461a74d33234a37b9e6875a9fa848d25b7307
Alexander Rohrig <me@alexanderrohrig.com>
2024-02-10 23:13:40 -0500

    Added Color Manager

--- yekaterinburg/Managers/ColorManager.swift
+++ yekaterinburg/Managers/ColorManager.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+import SwiftUI
+import OSLog
+
+class ColorManager {
+    static let shared = ColorManager()
+    func getGameCardColor(colorScheme: ColorScheme) -> Color {
+        switch colorScheme {
+        case .dark:
+            #if os(macOS)
+            return Color(nsColor: NSColor.darkGray)
+            #else
+            return Color(UIColor.darkGray)
+            #endif
+        case .light:
+            return Color(lightestGray)
+        @unknown default:
+            logger.error("Found unknown color scheme")
+            return Color.clear
+        }
+    }
+    private let logger = Logger(subsystem: GeneralSecretary.shared.subsystem, category: "ColorManager")
+    let lightestGray = "LightestGray"
+    private init() {}
+}
