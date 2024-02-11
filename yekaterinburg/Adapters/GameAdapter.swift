39b815eaaedee6dfe9a6b486e8434a87392fc80f
Alexander Rohrig <me@alexanderrohrig.com>
2024-02-10 23:07:58 -0500

    Added hockey, basketball, and college football to Game functions

--- yekaterinburg/Adapters/GameAdapter.swift
+++ yekaterinburg/Adapters/GameAdapter.swift
@@ -8,8 +8,63 @@
 import Foundation
 
 struct GameAdapter {
-    
+    /// Returns Game given game from MLB API
     static func getGameFrom(_ game: ResponseDateGame) -> Game {
         return Game(game: game)
     }
+    /// Returns Game given game from NHL API
+    static func getGameFrom(_ game: HockeyResponse.NHLDate.NHLGame) -> Game {
+        return Game(gameID: game.gamePk,
+                    homeTeam: game.teams.home.team.id,
+                    homeTeamName: game.teams.home.team.name,
+                    homeTeamCode: "", // TODO: Home team code
+                    homePoints: game.teams.home.score,
+                    awayTeam: game.teams.away.team.id,
+                    awayTeamName: game.teams.away.team.name,
+                    awayTeamCode: "", // TODO: Away team code
+                    awayPoints: game.teams.away.score,
+                    date: game.gameDate,
+                    status: game.status.detailedState,
+                    televisionOptions: "", // TODO: Television
+                    radioOptions: "", // TODO: Radio
+                    venue: "", // TODO: Venue
+                    type: .game(.hockey))
+    }
+    /// Returns Game given game from NBA API
+    static func getGameFrom(_ game: BasketballResponse.BBallGame) -> Game {
+        return Game(gameID: game.id,
+                    homeTeam: game.home_team.id,
+                    homeTeamName: game.home_team.city,
+                    homeTeamCode: game.home_team.abbreviation,
+                    homePoints: game.home_team_score,
+                    awayTeam: game.visitor_team.id,
+                    awayTeamName: game.visitor_team.city,
+                    awayTeamCode: game.visitor_team.abbreviation,
+                    awayPoints: game.visitor_team_score,
+                    date: game.date,
+                    status: "", // TODO: Television
+                    televisionOptions: "", // TODO: Television
+                    radioOptions: "", // TODO: Radio
+                    venue: "", // TODO: Venue
+                    type: .game(.basketball))
+    }
+    /// Returns Game given game from NCAA Football API
+    static func getGameFrom(_ game: CollegeFootballGame) -> Game {
+        return Game(gameID: game.id,
+                    homeTeam: game.home_id,
+                    homeTeamName: game.home_team,
+                    homeTeamCode: "", // TODO: Home team code
+                    homePoints: game.home_points,
+                    awayTeam: game.away_id,
+                    awayTeamName: game.away_team,
+                    awayTeamCode: "", // TODO: Away team code
+                    awayPoints: game.away_points,
+                    date: game.start_date,
+                    status: "", // TODO: Television
+                    televisionOptions: "", // TODO: Television
+                    radioOptions: "", // TODO: Radio
+                    venue: game.venue,
+                    type: .game(.collegeFootball))
+    }
+    private init() {}
 }
