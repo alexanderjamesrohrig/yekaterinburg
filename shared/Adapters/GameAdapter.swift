7844031b17ba6762becbbe339c1bff4e34778f62
Alexander Rohrig <me@alexanderrohrig.com>
2024-02-17 12:25:16 -0500

    Updates baseball and calcio adapters

--- shared/Adapters/GameAdapter.swift
+++ shared/Adapters/GameAdapter.swift
@@ -6,11 +6,28 @@
 //
 
 import Foundation
+import OSLog
+
+fileprivate let logger = Logger(subsystem: GeneralSecretary.shared.subsystem, category: "GameAdapter")
 
 struct GameAdapter {
     /// Returns Game given game from MLB API
     static func getGameFrom(_ game: ResponseDateGame) -> Game {
-        return Game(game: game)
+        return Game(gameID: game.gamePk,
+                    homeTeam: game.teams.home.team.id,
+                    homeTeamName: game.teams.home.team.franchiseName,
+                    homeTeamCode: "",
+                    homePoints: 0,
+                    awayTeam: game.teams.away.team.id,
+                    awayTeamName: game.teams.away.team.franchiseName,
+                    awayTeamCode: "",
+                    awayPoints: 0,
+                    date: DateAdapter.dateFromAPI(date: game.gameDate),
+                    status: game.status.detailedState,
+                    televisionOptions: "",
+                    radioOptions: "",
+                    venue: "",
+                    type: .game(.baseball))
     }
     /// Returns Game given game from NHL API
     static func getGameFrom(_ game: HockeyResponse.NHLDate.NHLGame) -> Game {
@@ -32,17 +49,23 @@ struct GameAdapter {
     }
     /// Returns Game given game from NBA API
     static func getGameFrom(_ game: BasketballResponse.BBallGame) -> Game {
+        var status = game.status ?? "Unknown"
+        var date = DateAdapter.dateFromShortString(date: game.date ?? "2001-08-09")
+        if status != "Final" {
+            date = DateAdapter.dateFromISO(date: status)
+            status = "Scheduled"
+        }
         return Game(gameID: game.id,
-                    homeTeam: game.home_team.id,
-                    homeTeamName: game.home_team.city,
-                    homeTeamCode: game.home_team.abbreviation,
-                    homePoints: game.home_team_score,
-                    awayTeam: game.visitor_team.id,
-                    awayTeamName: game.visitor_team.city,
-                    awayTeamCode: game.visitor_team.abbreviation,
-                    awayPoints: game.visitor_team_score,
-                    date: game.date,
-                    status: "", // TODO: Television
+                    homeTeam: game.home_team?.id ?? 0,
+                    homeTeamName: game.home_team?.city ?? "",
+                    homeTeamCode: game.home_team?.abbreviation ?? "",
+                    homePoints: game.home_team_score ?? 0,
+                    awayTeam: game.visitor_team?.id ?? 0,
+                    awayTeamName: game.visitor_team?.city ?? "",
+                    awayTeamCode: game.visitor_team?.abbreviation ?? "",
+                    awayPoints: game.visitor_team_score ?? 0,
+                    date: date,
+                    status: status,
                     televisionOptions: "", // TODO: Television
                     radioOptions: "", // TODO: Radio
                     venue: "", // TODO: Venue
@@ -65,6 +88,41 @@ struct GameAdapter {
                     radioOptions: "", // TODO: Radio
                     venue: game.venue,
                     type: .game(.collegeFootball))
+    }
+    static func getGameFrom(footballGame game: WorldFootballResponse.Match) -> Game {
+        // TODO: Fill remaining
+        return Game(gameID: game.id,
+                    homeTeam: 0,
+                    homeTeamName: game.homeTeam.shortName,
+                    homeTeamCode: "",
+                    homePoints: 0,
+                    awayTeam: 0,
+                    awayTeamName: game.awayTeam.shortName,
+                    awayTeamCode: "",
+                    awayPoints: 0,
+                    date: DateAdapter.dateFromISO(date: game.utcDate),
+                    status: game.status,
+                    televisionOptions: "",
+                    radioOptions: "",
+                    venue: "",
+                    type: .game(.calcio))
+    }
+    static func getGameFrom(baseballGame game: BaseballResponse.MLBDate.MLBGame) -> Game {
+        return Game(gameID: game.gamePk,
+                    homeTeam: game.teams.home.team.id,
+                    homeTeamName: game.teams.home.team.name,
+                    homeTeamCode: "",
+                    homePoints: 0,
+                    awayTeam: game.teams.away.team.id,
+                    awayTeamName: game.teams.away.team.name,
+                    awayTeamCode: "",
+                    awayPoints: 0,
+                    date: DateAdapter.dateFromISO(date: game.gameDate),
+                    status: "",
+                    televisionOptions: "",
+                    radioOptions: "",
+                    venue: "",
+                    type: .game(.baseball))
     }
     private init() {}
 }
