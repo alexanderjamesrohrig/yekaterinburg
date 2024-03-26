0ee7b1f0c2368787dc4900bd6e9510789aa4303e
Alexander Rohrig <me@alexanderrohrig.com>
2024-03-26 16:15:00 -0400

    Removed iOS widget

---
+++ shared/Views/BackgroundView.swift
@@ -0,0 +1,18 @@
+//
+//  BackgroundView.swift
+//  yekaterinburg
+//
+//  Created by Alexander Rohrig on 3/15/24.
+//
+
+import SwiftUI
+
+struct BackgroundView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BackgroundView()
+}
